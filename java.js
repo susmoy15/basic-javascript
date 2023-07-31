82af5763083548ed408a8072ea696fdd2d1b3f6b
susmoy15 <107901530+susmoy15@users.noreply.github.com>
2023-07-31 18:55:16 +0600

    first

---
+++ java.js
@@ -0,0 +1,12 @@
+console.log(7)
+// numeric
+var price = 56 ;
+var value = 9 ;
+
+// string
+ var name = " Susmoy bless "
+var location = "Goshaipur er don "
+
+// Boolean
+var serious = true;
+var isSingle = false;
---
+++ operation.js
@@ -0,0 +1,23 @@
+var onionPrice = 40;
+var tomatoPrice = 50;
+console.log(onionPrice + tomatoPrice);
+
+// concatination
+
+var name2 = "susmoy";
+var name3 = ' bless';
+Name = name2 + name3 ;
+console.log(Name);
+
+// typeof is a kind of number,string,boolean 
+
+var position = 'First';
+console.log(typeof position);
+
+var isHappy = true;
+console.log(typeof isHappy); 
+
+var num2 = 0.2;
+var num3 = 0.2;
+
+console.log(num2+num3);
