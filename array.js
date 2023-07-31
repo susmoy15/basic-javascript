37a5ade52a5e2fc4feabbf0d4db24fe8da545af0
susmoy15 <107901530+susmoy15@users.noreply.github.com>
2023-07-31 22:27:33 +0600

    Done array

--- array.js
+++ array.js
@@ -21,3 +21,61 @@ if(money>chicken_fry){
 else{
     console.log("Fokir ami");
 }
+
+// And Or operation:
+var money=500;
+var chicken=900;
+if(chicken=>300 && money>=400){
+    console.log("Pera nai chill")
+}
+else{
+    console.log("onek pera!!!")
+}
+
+var money=200;
+var chicken=500;
+if(chicken<300 || money>400){
+    console.log("Pera nai chill")
+}
+else{
+    console.log("onek pera!!!")
+}
+
+// multi-condition
+
+var cha = 20;
+var biri = 23;
+var coke =45;
+var coffee = 60;
+var sorbot = 8;
+
+if(sorbot>coffee){
+    console.log("Valo Hoye jao")
+}
+else if(coke>coffee){
+    console.log("Khamu na!!!")
+}
+else if(biri>sorbot){
+    console.log("Loss project")
+}
+else{
+    console.log("Jibon manei Zee Bangla")
+}
+
+
+// nested loop
+
+var math = true;
+var bangla = false;
+var english = true;
+if(math == true && bangla == true){
+    if(english == true){
+        console.log("Good")
+    }
+    else{
+        console.log("Bad")
+    }
+}
+else{
+    console.log("Bolo hori,hori bol")
+}
