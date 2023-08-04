a33093c1f98e78b369e73d4d375c8ab4882cafd2
susmoy15 <107901530+susmoy15@users.noreply.github.com>
2023-08-04 12:23:25 +0600

    for loop

--- while.js
+++ while.js
@@ -1,6 +1,13 @@
+// /while loop
 var chocolate = 0;
 while(chocolate < 6){
     console.log("Diye Dao");
     console.log (chocolate);
     chocolate++;
+}
+
+// for loop
+for (var i=0; i<=6; i++ ){
+    console.log(i);
+    console.log("Shei Tess");
 }
