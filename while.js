1d06a62975432b6c40665d1ed8e7aa60378c14ce
susmoy15 <107901530+susmoy15@users.noreply.github.com>
2023-08-04 12:42:44 +0600

    array loops

--- while.js
+++ while.js
@@ -11,3 +11,10 @@ for (var i=0; i<=6; i++ ){
     console.log(i);
     console.log("Shei Tess");
 }
+
+// display every elements of an array
+var numbers = [3,6,9,8,2,7];
+for(var i = 0 ; i<numbers.length ; i++){
+    var number = numbers[i];
+    console.log(number);
+}
