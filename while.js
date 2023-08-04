959cc8520fd9c449e6d2606f956bb2bc3cca8768
susmoy15 <107901530+susmoy15@users.noreply.github.com>
2023-08-04 13:24:01 +0600

    All done

--- while.js
+++ while.js
@@ -18,3 +18,24 @@ for(var i = 0 ; i<numbers.length ; i++){
     var number = numbers[i];
     console.log(number);
 }
+
+// break,continue
+
+var numbers = [30,64,95,84,22,73];
+for(var i = 0 ; i<numbers.length ; i++){
+    var number = numbers[i];
+    if(number < 73){
+        continue;       //break
+    }
+    console.log(number);
+}
+
+// Array update
+var fruits = ['Apple','Banana','Orange'];
+var BananaIndex = fruits.indexOf('Banana');
+fruits[BananaIndex] = 'Mango';
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
