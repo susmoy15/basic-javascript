7ee44daf8f5b5edeb184d343c8acc067ca14f936
susmoy15 <107901530+susmoy15@users.noreply.github.com>
2023-08-04 21:58:37 +0600

    Function deereturn details

--- function.js
+++ function.js
@@ -15,3 +15,24 @@ function add(num2,num3){
 } 
 
 add(4,5);
+
+
+function add(num2,num3){
+    console.log("Going to add:",num2,num3);
+    num = num2 + num3;
+    return num;
+} 
+
+add(4,5);
+console.log(num);
+
+function getAverage(assingment2 ,assingment3, assingment4){
+    var total = assingment2 + assingment3 + assingment4;
+    var average = total/3;
+    return average;
+}
+var assingment2 = 48;
+var assingment3 = 59;
+var assingment4 = 60; 
+var myAverage  = getAverage(assingment2,assingment3,assingment4 );
+console.log(myAverage); 
